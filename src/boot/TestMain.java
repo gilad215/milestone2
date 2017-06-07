b62bb6e284727a7c97f8b3624ba2e35b80972baa
G-lad <gilad215@gmail.com>
2017-06-07 21:54:58 +0300

    searchLib + strips 20th  After this commit, trying to split

--- src/boot/TestMain.java
+++ src/boot/TestMain.java
@@ -20,40 +20,9 @@ public class TestMain {
         SokoPredicate p3 = new SokoPredicate("boxAt", "?", "6,3");
 
 
-        MySokobanLoader loader = new MySokobanLoader("C:\\Users\\G-lad\\IdeaProjects\\milestone2\\Extras\\levelplannable.txt");
+        MySokobanLoader loader = new MySokobanLoader("C:\\Users\\G-lad\\IdeaProjects\\milestone2\\Extras\\level2.txt");
         loader.load();
-//        SokobanPlannable sokobanPlannable=new SokobanPlannable(loader.getLvl());
-//        Clause goals=sokobanPlannable.getGoal();
-//        System.out.println(goals.getValue());
-//
-//        Point test=new Point(3,1);
-        //SokobanSearchable<Point> searchable=new SokobanSearchable<>(loader.getLvl(),"box",new Point(2,2),new Point(1,3));
-//        SokobanSearchable<Point> searchable=new SokobanSearchable<>(loader.getLvl(),"box",new Point(2,2),new Point(1,3));
-//
-//        BFS<Point> searcher=new BFS<>();
-//        Solution sol=searcher.search(searchable);
-//        System.out.println(sol.getActions().toString());
-
-
         SokobanPlannable plannable = new SokobanPlannable(loader.getLvl());
-//        List<Action> actions = plannable.getSatisfyingActions(p2);
-//
-//        MySokobanDisplay display = new MySokobanDisplay(plannable.getLevel());
-//        display.display();
-//        System.out.println("INITIAL KB, size=" + plannable.getKnowledgeBase().getPredicates().size());
-//        System.out.println(plannable.getKnowledgeBase().toString());
-//        int i = 0;
-//        for (Action a : actions) {
-//            System.out.println("ACTION:" + i++);
-//            //System.out.println("PRECONDITIONS:"+a.getPreconditions().toString());
-//            System.out.println("EFFECTS:" + a.getEffects().toString());
-//            System.out.println("~~~~~~~~~~~~~~~");
-//            plannable.getKnowledgeBase().update(a.getEffects());
-//            System.out.println(plannable.getKnowledgeBase().toString());
-//            System.out.println("~~~~~~~~~~~~~~~");
-//            System.out.println("UPDATED KB SIZE: " + plannable.getKnowledgeBase().getPredicates().size());
-//            if (plannable.getKnowledgeBase().satisfies(p2)) System.out.println("SATISFIED");
-//        }
 
         Planner planner = new strips();
         System.out.println(plannable.getGoal().toString());
@@ -63,6 +32,7 @@ public class TestMain {
         for (Action a:stripsactions) {
             System.out.println("Action:"+i);
             System.out.println(a.getEffects().toString());
+
             if(a.getAct()!=null) finalActions.add(a.getAct());
             i++;
         }
